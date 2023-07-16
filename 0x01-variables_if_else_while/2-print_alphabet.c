6f951e88c6aa2c5b733d9baa71b99dc217f80191
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-16 15:41:15 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	for (int i = 97; i < 132; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
