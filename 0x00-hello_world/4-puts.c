1e800688e7aaf7916f2851ec9ac260262927c8fd
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-15 17:18:49 -0700

    Hello, puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-* Description: output \"Programming is like building a multilingual puzzle
-* return: (0)
-*/
+ * main - Entry point
+ * Description: 'first C code in ALX program'
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
