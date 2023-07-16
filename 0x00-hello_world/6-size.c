45d466e0bae93fe400f7b249a805965be2f50044
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-15 17:39:25 -0700

    Size is not grandeur, and territory does not make a nation task

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\nSize of a int: %zu byte(s)\nSize of a long int: %zu byte(s)\nSize of a long long int: %zu byte(s)\nSize of a float: %zu byte(s)\n", sizeof(char), sizeof(int), sizeof(long int), sizeof(long long int), sizeof(float));
+	return (0);
+}
