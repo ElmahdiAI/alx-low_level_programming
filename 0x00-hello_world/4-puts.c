ab49d1a5f1837485a0f712f3e2fe4b57641d0fbe
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-15 17:13:20 -0700

    Hello, puts task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,7 +2,7 @@
 int main(void)
 {
 	/*
-	 * Description: will output the sentence \"Programming is like building a multilingual puzzle
+	 * Description: output \"Programming is like building a multilingual puzzle
 	 * return: (0)
 	 */
 	puts("\"Programming is like building a multilingual puzzle");
