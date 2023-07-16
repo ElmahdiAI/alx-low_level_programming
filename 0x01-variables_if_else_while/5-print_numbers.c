a217767d63e8e676e96fe977bb78542ad8a2a403
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-16 16:58:01 -0700

    Numbers task

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9: i++)
+		printf("%d\n", i);
+	return (0);
+}
