d739e49ea3dbdc8925a03f4d4b229b559724b446
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-16 16:33:50 -0700

    alphABET task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        char c, s;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                putchar(c);
+        }
+	for (s = 'A'; s <= 'Z'; s++)
+	{
+		putchar(s);
+	}
+        putchar('\n');
+        return (0);
+}
