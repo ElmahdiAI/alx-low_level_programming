9b16e595dd8167e9344f60ca51ea60ed0e1836a0
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-17 16:18:37 -0700

    Hexadecimal task

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char c;
+
+	while (i <= 9)
+	{
+		putchar(i);
+		i++;
+	}
+	for (c = 'A'; c <= 'F'; i++)
+		putchar(c);
+	putchra('\n');
+	return (0);
+}
