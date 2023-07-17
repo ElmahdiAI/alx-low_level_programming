5a9090c234f5098c2fe2abb0598a6273fe8806b3
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-17 16:58:11 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need task

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,j;
+	for (i = 0; i < 9; i++)
+	{
+		for (j = i+1; j < 9; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
