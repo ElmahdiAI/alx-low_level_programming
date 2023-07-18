506f1aa711fc08152b7b5162e5ea77afecff517b
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-17 17:17:35 -0700

    The success combination in business is: Do what you do better... and: do more of what you do... task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'another C code in ALX program'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = i + 1; j <9; j++)
+		{
+			for (k = j + 1; k <= 9)
+			{
+				putchar('0' + i);
+				putchar('0' + j);
+				putchar('0' + j);
+				if (i == 7 && j == 8 && k == 9)
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
