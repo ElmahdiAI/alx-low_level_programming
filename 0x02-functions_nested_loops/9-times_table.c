da8703cba196abb3f03077ca11d26c32cd8e7888
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-19 09:58:46 -0700

    update

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *  times_table - check the code
+ * Return: Success 0.
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			_putchar(i * j + '0');
+			_putchar('\n');
+		}
+	}
+}
