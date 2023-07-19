7ed19bc00a45716367f0f62bc9d305b85390658f
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-19 16:23:11 -0700

    update

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  print_to_98- sum of two integers
+ * @n: start number
+ * Return: Success 0.
+ */
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d", n);
+	}
+}
