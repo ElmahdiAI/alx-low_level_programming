8b0e1678e4d6445ec4e9430f50f797ed115d1f6a
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-21 18:33:28 -0700

    update

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,13 +14,12 @@ void print_triangle(int size)
 		{
 			for ((b = size - a); b > 0; b--)
 				_putchar(' ');
-
 			for (b = 0; b < a; b++)
 				_putchar('#');
 			if (a == size)
 				continue;
 			_putchar('\n');
 		}
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
