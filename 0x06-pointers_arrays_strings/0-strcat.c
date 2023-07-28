0e1c0a11c5e804e794b9c3497db1b97857dc87fb
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-28 16:45:06 -0700

    update

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,12 +1,10 @@
 #include "main.h"
-/*******************************************************
- *********************MEHDI**AI*************************
- * _strcat - concatenates two strings
- * @src: string source
- * @dest: string destinataire
- * Return: returns poiner
- *******************************************************
- *******************************************************/
+ /*
+ *_strcat - concatenates two strings
+ *@src: string source
+ *@dest: string destinataire
+ *Return: returns poiner
+ */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0, len = 0;
