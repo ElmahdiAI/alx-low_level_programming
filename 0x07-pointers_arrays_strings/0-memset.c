dd6f52ef7fa1b70afd499601d282c302ec19865c
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-31 16:58:35 -0700

    update

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*_memset -  a function that fills
+* the first n bytes of the memory area
+* pointed to s with the constant byte b
+*@s:target
+*@b: constant byte
+*@n:number of byte
+*Return: returns new value of target
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
+}
