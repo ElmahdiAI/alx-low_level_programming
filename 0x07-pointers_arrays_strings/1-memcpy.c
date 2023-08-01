e30c5983d052f64adcb8e7b28a3723e1d76c4fa8
Mehdi_AI <mehdiaititto239@gmail.com>
2023-07-31 17:24:21 -0700

    update

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -6,11 +6,11 @@
  * *@src:area where bytes are copied from
  * *@n:number of bytes to copy
  * *Return: returns a pointer to n
- * */
+ */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
-	
+
 	for (; i < n; i++)
 		dest[i] = src[i];
 	return (dest);
