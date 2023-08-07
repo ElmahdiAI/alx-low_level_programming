b98528f2872b7d0252af3d39b65b68ed8711b1d2
Mehdi_AI <mehdiaititto239@gmail.com>
2023-08-07 08:04:41 -0700

    update

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,14 +1,14 @@
 #include "main.h"
+
 /*******************************************************
  *********************MEHDI**AI*************************
- * _strncat- concatenates two strings
+ * _strncat - concatenates two strings
  * @src: string source
  * @dest: string destinataire
  * @n : bytes to take from src
  * Return: returns new concatenated string
  *******************************************************
- *******************************************************
- */
+ *******************************************************/
 char *_strncat(char *dest, char *src, int n)
 {
 	int i = 0, len = 0;
