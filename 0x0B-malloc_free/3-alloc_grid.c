d294161720a18a8f81d849ea2977c0744d8e0a35
Mehdi_AI <mehdiaititto239@gmail.com>
2023-08-09 16:04:41 -0700

    update

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,30 +10,31 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, j, **grid;
+	int **array, i, j;
+	int len = width * height;
 
-	if (width <= 0 || height <= 0)
+	if (len <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int *) * width);
-	if (grid == NULL)
+
+	array = (int **)malloc(sizeof(int *) * height);
+	if (array == NULL)
 		return (NULL);
+
 	for (i = 0; i < height; i++)
 	{
-		grid[i] = (int *)malloc(sizeof(int) * height);
-		if (grid[i] != NULL)
+		array[i] = (int *)malloc(sizeof(int) * width);
+		if (array[i] == NULL)
 		{
-			for (j = 0; j < width; j++)
-			{
-				grid[i][j] = 0;
-			}
-		}
-		else
-		{
-			for (i = 0; i < height; i++)
-				free(grid[i]);
-			free(grid);
+			for (i--; i >= 0; i--)
+				free(array[i]);
+			free(array);
 			return (NULL);
 		}
 	}
-	return (grid);
+
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+
+	return (array);
 }
