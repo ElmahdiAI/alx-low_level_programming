bd26f41301294b1e9473828c54fbeeb0ae58190a
Mehdi_AI <mehdiaititto239@gmail.com>
2023-08-09 11:46:16 -0700

    update

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -23,5 +23,5 @@ char *create_array(unsigned int size, char c)
 	{
 		array[i] = c;
 	}
-	ireturn (array);
+	return (array);
 }
--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -10,18 +10,30 @@
  */
 int **alloc_grid(int width, int height)
 {
-	int i, **grid;
+	int i, j, **grid;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int) * width);
+	grid = (int **)malloc(sizeof(int *) * width);
 	if (grid == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
+	for (i = 0; i < height; i++)
 	{
 		grid[i] = (int *)malloc(sizeof(int) * height);
-		if (grid[i] == NULL)
+		if (grid[i] != NULL)
+		{
+			for (j = 0; j < width; j++)
+			{
+				grid[i][j] = 0;
+			}
+		}
+		else
+		{
+			for (i = 0, i < height; i++)
+				free(grid[i]);
+			free(grid);
 			return (NULL);
+		}
 	}
 	return (grid);
 }
