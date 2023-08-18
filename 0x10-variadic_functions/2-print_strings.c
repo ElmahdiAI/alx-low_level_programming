e9ef640f8816378f95b07e258d9d52946b997849
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-17 17:31:47 -0700

    update

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_strings- a function that prints strings, followed by a new line.
+ * @separator:  the string to be printed between the strings
+ * @n: the number of integers passed to the function
+ * Return: Nothing.
+*/
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *store;
+	va_list args;
+
+	if (n == 0)
+		return;
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		store = va_arg(args, char*);
+		if (store != NULL)
+			printf("%s", store);
+		else
+		{
+			printf("(nil)");
+		}
+		if (separator != NULL && i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all-  a function that prints anything.
+ * @format:  a list of types of arguments passed to the function
+ * it is a constant pointer that points to a constant string. 
+ * The string pointed to by format cannot be modified, and the pointer 
+ * format itself cannot be changed to point to a different memory location.
+ * Return: Nothing.
+*/
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, flag = 1;
+	char *store;
+
+	va_start(args, format);
+	while (format[i] != '\0')
+	{
+		switch (format[i])
+		{
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			store = va_arg(args, char*);
+			if (store != NULL)
+				printf("%s", store);
+			else
+				printf("(nil)");
+		break;
+		default:
+			flag = 0;
+			break;
+		}
+		if (format[i + 1] != '\0' && flag == 0)
+			printf(", ");
+		i++;
+	}
+	va_end(args);
+	printf("\n");
+}
