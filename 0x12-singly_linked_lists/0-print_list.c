9db63fe0488bd0b5a49e825f30dcaa4e96b9ec1f
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-24 16:41:45 -0700

    update

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,10 +14,11 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
+		{
 			printf("[%u] %s", h->len, h->str);
-
-		h = h->next;
-		n++;
+			h = h->next;
+			n++;
+		}
 	}
 	return (n);
 }
