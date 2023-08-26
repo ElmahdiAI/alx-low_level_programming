695771eb752327c3506baa7c3c62785acb9efdf5
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-26 12:52:19 -0700

    updated

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  * wildcmp - compares two strings with wildcard pattern
  * @s1: the first string
@@ -8,17 +7,14 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-        return 1;
-
-    if (*s2 == '*')
-    {
-        if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
-            return 1;
-    }
-
-    if (*s1 == *s2)
-        return wildcmp(s1 + 1, s2 + 1);
-
-    return 0;
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || (*s1 != '\0' && wildcmp(s1 + 1, s2)))
+			return (1);
+	}
+	if (*s1 == *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return (0);
 }
