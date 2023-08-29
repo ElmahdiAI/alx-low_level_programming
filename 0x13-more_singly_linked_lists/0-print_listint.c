a5289ef7538023716f8f66025112b1f949ee4753
ElmahdiAI <mehdiaititto239@gmail.com>
2023-08-29 16:33:38 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -4,13 +4,18 @@
  * print_listint- a function that prints all the elements of a listint_t list.
  * @h: provided list
  * Return: the number of nodes
-*/
+ */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *temp;
 	int count = 0;
 
 	temp = h;
+	if (!temp)
+	{
+		printf("(nil)\n");
+		return (0);
+	}
 	while (temp)
 	{
 		printf("%d\n", temp->n);
